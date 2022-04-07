c5feb99938dfcfcd4344ef88a077f834f8a2daa0
Jin <jin@JindeMBP.lan>
2022-04-07 13:58:52 +0800

    prepare unit test

---
+++ dep-version/src/main/kotlin/com/easy/version/dependencies/Tester.kt
@@ -0,0 +1,6 @@
+package com.easy.version.dependencies
+
+object Tester {
+    const val mockito_kotlin = "org.mockito.kotlin:mockito-kotlin:4.0.0"
+    const val coroutines_test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1"
+}
