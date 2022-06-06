bf27491962c9bc9bf8825a16d155ff7f30587442
BreakZero <dejin.po@gmail.coom>
2022-06-06 11:18:31 +0800

    add ktlint checking code style

--- assets/data/build.gradle.kts
+++ assets/data/build.gradle.kts
@@ -3,10 +3,6 @@ import com.easy.version.hiltDependencies
 import com.easy.version.roomDependencies
 import com.easy.version.unitTestDependencies
 
-kotlinter {
-    disabledRules = arrayOf("no-wildcard-imports", "filename")
-}
-
 dependencies {
     implementation(project(":core"))
     implementation(project(":assets:domain"))
